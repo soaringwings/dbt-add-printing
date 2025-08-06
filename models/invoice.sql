4a6d3c909fafe650b4ac9c810fcf159ca462512a
Tang Yu <addprinting01@gmail.com>
2025-08-06 20:12:36 +0000

    create invoice model

---
+++ models/invoice.sql
@@ -0,0 +1,21 @@
+with invoice as (
+    select * from splendid-strand-463617-k9.quickbooks.invoice
+),
+
+item as (
+    select * from splendid-strand-463617-k9.quickbooks.item
+), 
+
+invoice_line as (
+    select * from splendid-strand-463617-k9.quickbooks.invoice_line
+),
+
+final as (
+    select
+        * from invoices
+
+    left join invoice_line
+        on invoice.id = invoice_line.invoice_id;
+)
+
+select * from final
